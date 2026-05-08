32fbc0cc4a4393a18df1ce217746bd3d897992b9
Mamta <mamtachauhan1294@gmail.com>
2026-05-08 20:27:26 +0530

    MongoDB atlas Corrected

--- Backend/Server.js
+++ Backend/Server.js
@@ -4,33 +4,33 @@ const storyRoutes = require("./routes/storyRoutes");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const scrapeNews = require("./Scraper/scrapeNews");
+require("dotenv").config();
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/stories", storyRoutes);
 app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("API Running");
 });
+
 app.get("/scrape", async (req, res) => {
-
   await scrapeNews();
-
   res.send("News Scraped Successfully");
-
 });
 
-mongoose.connect("mongodb://127.0.0.1:27017/newsDB")
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
 
   console.log("MongoDB Connected");
 
-
   app.listen(PORT, () => {
-    console.log("Server running on port 5000");
+    console.log(`Server running on port ${PORT}`);
   });
 
 })

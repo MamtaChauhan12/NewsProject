d0607a0ad1a715bbf06a997c7556d9d6f177f31e
Mamta <mamtachauhan1294@gmail.com>
2026-05-08 20:00:27 +0530

    Port corrected

--- Backend/Server.js
+++ Backend/Server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const scrapeNews = require("./Scraper/scrapeNews");
 const app = express();
-
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -29,7 +29,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/newsDB")
   console.log("MongoDB Connected");
 
 
-  app.listen(5000, () => {
+  app.listen(PORT, () => {
     console.log("Server running on port 5000");
   });
 
